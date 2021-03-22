9fc497f0e4511867ff70e5cf52c15571ee2293dc
phytechno <phytechno@gmail.com>
2021-03-22 13:16:15 +0900

    Version1

---
+++ Assets/ClippingController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClippingController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }    void OnTriggerEnter(Collider other)
+    {
+        // 衝突したコイン・コーン・カーをDestroyする。
+        if(other.gameObject.tag == "CoinTag" || other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
